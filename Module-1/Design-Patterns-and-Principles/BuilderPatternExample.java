0838317f983bfd6c4e3d2338db8c70e2d607332c
Venu Dadi <venudadi11@gmail.com>
2026-06-23 18:58:17 +0530

    Update BuilderPatternExample.java

--- Module-1/Design-Patterns-and-Principles/BuilderPatternExample.java
+++ Module-1/Design-Patterns-and-Principles/BuilderPatternExample.java
@@ -1,20 +1,69 @@
 public class BuilderPatternExample {
-    static c]ass Computer {
-        private String CPU;
-        private String RAM;
+
+    // Product Class
+    static class Computer {
+        private String cpu;
+        private String ram;
         private String storage;
 
-        private Computer(ComputerBuilder builder) {
-            this.CPU = builder.CPU;
-            this.RAM = builder.RAM;
+        // Private constructor
+        private Computer(Builder builder) {
+            this.cpu = builder.cpu;
+            this.ram = builder.ram;
             this.storage = builder.storage;
         }
 
-        public void displaySpecs() {
-            System.out.println("Computer Specifications:");
-            System.out.println("CPU: " + CPU);
-            System.out.println("RAM: " + RAM);
+        public void displayConfiguration() {
+            System.out.println("Computer Configuration:");
+            System.out.println("CPU: " + cpu);
+            System.out.println("RAM: " + ram);
             System.out.println("Storage: " + storage);
+            System.out.println();
         }
+
+        // Static Nested Builder Class
+        static class Builder {
+            private String cpu;
+            private String ram;
+            private String storage;
+
+            public Builder setCPU(String cpu) {
+                this.cpu = cpu;
+                return this;
+            }
+
+            public Builder setRAM(String ram) {
+                this.ram = ram;
+                return this;
+            }
+
+            public Builder setStorage(String storage) {
+                this.storage = storage;
+                return this;
+            }
+
+            public Computer build() {
+                return new Computer(this);
+            }
+        }
+    }
+
+    // Test Class
+    public static void main(String[] args) {
+
+        Computer gamingPC = new Computer.Builder()
+                .setCPU("Intel Core i9")
+                .setRAM("32GB")
+                .setStorage("1TB SSD")
+                .build();
+
+        Computer officePC = new Computer.Builder()
+                .setCPU("Intel Core i5")
+                .setRAM("16GB")
+                .setStorage("512GB SSD")
+                .build();
+
+        gamingPC.displayConfiguration();
+        officePC.displayConfiguration();
     }
 }
